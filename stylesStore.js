9410cdf8f80636f06cd4e6fa0331800dc31a14a0
christopher-1989 <64772696+christopher-1989@users.noreply.github.com>
2021-06-07 18:47:37 +1000

    Rewriting display as flexbox. Refactoring data store as .js file

---
+++ stylesStore.js
@@ -0,0 +1,42 @@
+const colourStyles = [
+    {
+        colour: "Black",
+        hex: "#232323"
+    },
+    {
+        colour: "White",
+        hex: "#fffe"
+    },
+    {
+        colour: "Aqua",
+        hex: "#14bdbd"
+    },
+    {
+        colour: "Teal",
+        hex: "#0b925e"
+    },
+    {
+        colour: "Grey",
+        hex: "#201F2E"
+    }
+]
+
+const fonts = ["pt-sans", "roboto", "helvetica"];
+
+const fontSizes = {
+    full: {
+        h1: "55px",
+        h2: "45px",
+        p: "24px"
+    },
+    tablet: {
+        h1: "50px",
+        h2: "35px",
+        p: "18px"
+    },
+    phone: {
+        h1: "40px",
+        h2: "24px",
+        p: "16px"
+    } 
+} 
