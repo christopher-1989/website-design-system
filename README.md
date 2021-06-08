2138f708c8f25ef5eb00955dc3451d5d6470be7c
christopher-1989 <64772696+christopher-1989@users.noreply.github.com>
2021-06-08 13:30:13 +1000

    Finished update including using flexbox and creating a store of templates within main.js

--- README.md
+++ README.md
@@ -2,11 +2,17 @@
 
 ## Introduction
 
-This project was intended for the creator to be able to store/showcase the basic design styles that would commonly be used when creating a website.
+This project is intended for the creator to be able to store/showcase the basic design styles that would commonly be used when creating a website.
 
 ## Boilerplate HTML
 
 A boilerplate-html file has been included. To view the contents of the file either load it directly from the repository or when on the home page of the website design system, click on the "Boilerplate HTML" button.
+
+## June 2021 update
+
+The layout of the website design system has been updated to include flexbox meaning that the page is more responsive and consistent than previous versions.
+
+A store of the templates has been created in main.js allowing for future additions or revisions to be greatly simplified. Each of the 'cards' for each of the three sections: colours, fonts, and font sizes are rendered from the main.js file. 
 
 ## Further information
 
--- js/main.js
+++ js/main.js
@@ -1,4 +1,95 @@
-import { colourStyles, fonts, fontSizes } from "./stylesStore.js";
-console.log(colourStyles);
-console.log(fonts);
-console.log(fontSizes);
+const colourStyles = [
+    {
+        colour: "Black",
+        hex: "#232323",
+        styles: "background-color: #232323; color: white; border: #232323 1px solid;"
+    },
+    {
+        colour: "White",
+        hex: "#fffe",
+        styles: "background-color: #ffffffee; border: 1px solid;"
+    },
+    {
+        colour: "Aqua",
+        hex: "#14bdbd",
+        styles: "background-color: #14bdbd; border: 1px solid;"
+    },
+    {
+        colour: "Teal",
+        hex: "#0b925e",
+        styles: "background-color: #0b925e; border: 1px solid;"
+    },
+    {
+        colour: "Grey",
+        hex: "#201F2E",
+        styles: "background-color: #201F2E; color: #fffe; border: #201F2E 1px solid;"
+    }
+]
+
+const fonts = [
+    {
+    font: "pt-sans",
+    fontFamily: 'font-family: "PT Sans", sans-serif;'
+    },
+     {
+         font: "roboto",
+         fontFamily: 'font-family: Roboto, sans-serif;'
+      },
+      {
+          font: "helvetica",
+        fontFamily: "font-family: Helvetica, sans-serif;"
+    }];
+
+const fontSizes = {
+    h1: {
+        heading: "Herosection heading",
+        fullSize: "55px",
+        tabletSize: "50px",
+        phoneSize: "38px"
+    },
+    h2: {
+        heading: "Herosection subheading",
+        fullSize: "45px",
+        tabletSize: "35px",
+        phoneSize: "24px"
+    },
+    h3: {
+        heading: "Second subheading",
+        fullSize: "30px",
+        tabletSize: "28px",
+        phoneSize: "20px"
+    },
+    p: {
+        heading: "Herosection paragraph",
+        fullSize: "22px",
+        tabletSize: "18px",
+        phoneSize: "16px"
+    }  
+}
+
+//logic for inserting colours into index.html
+let fullHTMLtext = "";
+colourStyles.forEach(colourObject => {
+    const card = `<div class="color-card" id="${colourObject.colour}" style="${colourObject.styles}">${colourObject.colour}<br>${colourObject.hex}</div>`;
+    fullHTMLtext += card;
+});
+
+document.getElementById("colours-card-container").innerHTML = fullHTMLtext
+
+//logic for inserting fonts into index.html
+fullHTMLtext = "";
+fonts.forEach(fontObject => {
+    const card = `<div class='font-card' id="${fontObject.font}" style="${fontObject.fontFamily}"><h3><u>${fontObject.font}</u></h3><p>STANDARD: The monkey crossed the road.</p><p><strong>BOLD: The monkey crossed the road.</strong></p><p><em>ITALICS: The monkey crossed the road.</em></p></div>`;
+    fullHTMLtext += card; 
+});
+
+document.getElementById('fonts-card-container').innerHTML = fullHTMLtext;
+
+//logic for inserting font styles into index.html
+fullHTMLtext = "";
+for (const [key, value] of Object.entries(fontSizes)) {
+    const card = `<div class='font-size-card'><h3 class="${key}">${value.heading}</h3><p>Font-size: ${value.fullSize}</p><p>At max screen of 960px: ${value.tabletSize}</p><p>At max screen of 768px: ${value.phoneSize}</p></div>`;
+    fullHTMLtext+= card;
+}
+
+document.getElementById('font-size-container').innerHTML = fullHTMLtext;
